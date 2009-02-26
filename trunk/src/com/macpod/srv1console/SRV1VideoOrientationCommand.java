3a1cb69fa214741a8987074b692bdf8630660ead
nelsonjm@macpod.net <nelsonjm@macpod.net@481d3d60-f604-11dd-aa46-671127399dce>
2009-02-26 03:16:09 +0000

    -Added video orientation command.

---
+++ trunk/src/com/macpod/srv1console/SRV1VideoOrientationCommand.java
@@ -0,0 +1,54 @@
+package com.macpod.srv1console;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+
+import android.util.Log;
+
+public class SRV1VideoOrientationCommand extends SRV1Command {
+	public static enum VideoOrientation {
+		NORMAL_ORIENTATION, FLIPPED_ORIENTATION
+	}
+
+	char orientation = 'Y';
+
+	public SRV1VideoOrientationCommand(VideoOrientation orientation) {
+
+		switch (orientation) {
+		case NORMAL_ORIENTATION:
+			this.orientation = 'Y';
+			break;
+		case FLIPPED_ORIENTATION:
+			this.orientation = 'y';
+			break;
+		}
+	}
+
+	public boolean process(DataInputStream in, DataOutputStream out)
+			throws Exception {
+
+		// Clear input stream in case there is data present.
+		clearInputStream(in);
+
+		// Write a byte to orient video capture.
+		out.writeByte(orientation);
+
+		// Verify video capture orientation was set.
+		if ((char) in.readByte() == '#' && (char) in.readByte() == orientation) {
+			Log.d("SRV1", "Set video orientation!");
+			return true;
+		}
+		Log.d("SRV1", "Could not set video orientation!");
+		return false;
+	}
+
+	public boolean repeat() {
+		return false;
+	}
+
+	@Override
+	public boolean repeatOnFail() {
+		return true;
+	}
+
+}
